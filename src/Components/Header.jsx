70b9eacfaaeb754bfb432cc1350f6229c0d2987b
Vexonor <vexonordark@gmail.com>
2025-06-09 04:34:33 +0700

    Make Navbar and ScrollToTop Button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   const navItems = [
     { label: 'Beranda', link: '/' },
     { label: 'Tentang', link: '/tentang' },
     { label: 'Pengurus', link: '/pengurus' },
     { label: 'Artikel', link: '/artikel' },
     { label: 'Kontak', link: '/kontak' },
-  ]
+  ];
+
   return (
     <header className="h-[6.25rem]">
       <nav className="size-full flex flex-wrap items-center justify-between">
@@ -19,33 +26,39 @@ const Header = () => {
         {/* Nav Items */}
         <ul className="flex-2 flex justify-center items-center gap-10">
           {
-            navItems.map((items, index) => (
+            navItems.map((item, index) => (
               <NavItems
                 key={index}
-                label={items.label}
-                link={items.link}
+                label={item.label}
+                link={item.link}
+                isActive={isActive(item.link)}
               />
             ))
           }
         </ul>
         {/* OSC Regist Button */}
-        <div className="flex-1 flex justify-center">
-          <Link>
+        <div className="flex-1 flex justify-center text-primary-1 font-semibold group">
+          <Link to="/osc" className="bg-accent-1 border-4 border-primary-1 px-4 py-2 rounded-full group-hover:scale-105 transition-all delay-300">
             Daftar OSC 2025
           </Link>
         </div>
       </nav>
     </header>
-  )
+  );
 };
 
-const NavItems = ({ label, link }) => {
+const NavItems = ({ label, link, isActive }) => {
   return (
-    <li className="text-black relative group flex justify-center">
-      <Link to={link}>{label}</Link>
-      <div className="absolute -bottom-1 bg-amber-300 w-0 group-hover:w-1/2 h-1 transition-all delay-150" />
+    <li className="text-primary-1 relative group flex justify-center">
+      <Link
+        to={link}
+        className={`${isActive ? 'font-bold' : 'font-normal'}`}
+      >
+        {label}
+      </Link>
+      <div className={`absolute -bottom-1 bg-accent-1 transition-all delay-150 h-1 ${isActive ? 'w-1/2' : 'w-0 group-hover:w-1/2'}`} />
     </li>
   );
-}
+};
 
 export default Header;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,24 @@ import Header from "../Components/Header";
 
 const LandingPage = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        {/* Isi Konten Lainnya */}
+        <ScrollToTop />
+      </main>
+    </>
   );
 };
+
+const ScrollToTop = () => {
+  return (
+    <div className="flex justify-center items-center absolute right-10 bottom-10 bg-accent-1 border-4 border-primary-1 rounded-xl size-[4.375rem]">
+      <img src="icon/ic_arrow_up.svg" alt="Arrow Up" />
+    </div>
+  )
+}
 
 export default LandingPage;
