02e48b99a44f08193209614a2357c9444054a8ee
EliteHero <bagassatrio807@gmail.com>
2025-06-14 21:14:51 +0700

    Fix code structures

--- src/Components/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Card = () => {
-  // Elsya Lanjutin dari sini
-  return <div className="bg-[#facc15]">Card</div>;
-};
-
-export default Card;
--- src/assets/LandingPage/index.js
+++ src/assets/LandingPage/index.js
@@ -1,3 +1,11 @@
 import blug from "./blug.png";
+import Bintang from './ProgramUnggulan/Bintang.png';
+import BintangBesar from './ProgramUnggulan/Bintang_Besar.png';
+import Garis from './ProgramUnggulan/Garis.png';
+import BelajarSiber from './ProgramUnggulan/Belajar_Siber.png';
+import Hacker from './ProgramUnggulan/Hacker.png';
+import Dot from './ProgramUnggulan/Dot.png';
+import BelajarProgramming from './ProgramUnggulan/Belajar_Programming.png';
+import Ngoding from './ProgramUnggulan/Ngoding.png';
 
-export { blug };
+export { blug, Bintang, BintangBesar, Garis, BelajarSiber, Hacker, Dot, BelajarProgramming, Ngoding };
