4f626fca2d6fdc968b26a6da4215015ee0c76c7d
michael3312301112 <liong888g@gmail.com>
2025-06-15 05:49:27 +0700

    komponen landing page bagian contact

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../Components/Header";
+import ContactSections from "../Components/contact_sections";
 
 const LandingPage = () => {
   return (
     <div>
-      <Header />
+      <ContactSections />
     </div>
   );
 };
