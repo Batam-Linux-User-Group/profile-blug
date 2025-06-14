ffe0c5c2f1b22180c7fb7a2d01b655992f64e0d4
EliteHero <bagassatrio807@gmail.com>
2025-06-14 22:46:04 +0700

    WIP: Merge Hero and VideoSection branch

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Components/Header";
 import Hero from "../Components/LandingPage/Hero";
+import VideoSection from "../Components/LandingPage/VideoSection";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <Hero title="BATAM LINUX USER GROUP" subTitle="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro accusamus, quae assumenda sed possimus aspernatur dolorem perspiciatis ipsam ea, nam maiores libero repudiandae sunt pariatur perferendis distinctio quo commodi."/>
+      <VideoSection />
     </div>
   );
 };
