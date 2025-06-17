d3b17aa40729c0854449227769fdf9b48d02e559
Bayu Maulana <bayufransdo@gmail.com>
2025-06-17 16:25:10 +0700

    fix typo in importing component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import Header from "../Components/Header";
-import Hero from "../Components/LandingPage/Hero";
-import VideoSection from "../Components/LandingPage/VideoSection";
-import TujuanSection from "../Components/LandingPage/TujuanSection";
-import ProgramUnggulan from "../Components/LandingPage/ProgramUnggulan";
-import Partnership from "../Components/LandingPage/Partnership";
-import Footer from "../Components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/LandingPage/Hero";
+import VideoSection from "../components/LandingPage/VideoSection";
+import TujuanSection from "../components/LandingPage/TujuanSection";
+import ProgramUnggulan from "../components/LandingPage/ProgramUnggulan";
+import Partnership from "../components/LandingPage/Partnership";
+import Footer from "../components/Footer";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
-      <Hero title="BATAM LINUX USER GROUP" subTitle="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro accusamus, quae assumenda sed possimus aspernatur dolorem perspiciatis ipsam ea, nam maiores libero repudiandae sunt pariatur perferendis distinctio quo commodi."/>
+      <Hero
+        title="BATAM LINUX USER GROUP"
+        subTitle="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro accusamus, quae assumenda sed possimus aspernatur dolorem perspiciatis ipsam ea, nam maiores libero repudiandae sunt pariatur perferendis distinctio quo commodi."
+      />
       <VideoSection />
       <Partnership />
       <TujuanSection />
       <ProgramUnggulan />
       <Footer />
     </div>
-
   );
 };
 
