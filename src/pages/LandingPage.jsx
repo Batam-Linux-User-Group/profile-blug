87d13229c7c1f050602a6a2aa1d57e277afe4d2f
fizzxyz <hafizxd24@gmail.com>
2025-06-25 10:50:47 +0700

    Hero Component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../Components/Header";
+<<<<<<< HEAD
 import Hero from "../Components/LandingPage/Hero";
+=======
+import HubungiKami from "../Components/HubungiKami";
+>>>>>>> a3d328d (add HubungiKami component and lucide-react dependency)
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
+<<<<<<< HEAD
       <Hero title="BATAM LINUX USER GROUP" subTitle="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro accusamus, quae assumenda sed possimus aspernatur dolorem perspiciatis ipsam ea, nam maiores libero repudiandae sunt pariatur perferendis distinctio quo commodi."/>
+=======
+      <HubungiKami />
+>>>>>>> a3d328d (add HubungiKami component and lucide-react dependency)
     </div>
   );
 };
