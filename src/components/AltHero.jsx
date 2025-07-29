cbdfccd1b9cefd2b5a5a113fa155327afb02ace4
Bayu Maulana <bayufransdo@gmail.com>
2025-07-29 23:01:13 +0700

    replace asset using import Tentang page

--- src/components/AltHero.jsx
+++ src/components/AltHero.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import background from "../assets/HubungiPage/background.png"; // Import tetap di sini
+import React from 'react';
+import background from '../assets/HubungiPage/background.png'; // Import tetap di sini
+import greenLine from '../assets/AltHero/line_green.png'; // Import tetap di sini
 
 const AltHero = ({ label, title, addStyle }) => {
   return (
@@ -7,29 +8,37 @@ const AltHero = ({ label, title, addStyle }) => {
       className="w-full bg-cover bg-center py-32 text-center text-black"
       style={{
         backgroundImage: `url(${background})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
       }}
     >
-      <div className="bg-yellow-400 bg-opacity-60 inline-block px-4 py-1 text-sm font-bold" data-aos="flip-down">
+      <div
+        className="bg-yellow-400 bg-opacity-60 inline-block px-4 py-1 text-sm font-bold"
+        data-aos="flip-down"
+      >
         {label}
       </div>
       <h2 className="text-3xl md:text-4xl font-black mt-4" data-aos="zoom-in">
         {title} <br />
         {(() => {
           switch (addStyle) {
-            case "tentang":
-              return(
+            case 'tentang':
+              return (
                 <div className="flex flex-col">
-                  <h2>Go <span className="text-accent-2">OPEN-SOURCE!</span></h2>
-                  <img src="src/assets/AltHero/line_green.png" alt="Green Line" className=" relative w-60 self-center left-5" />
+                  <h2>
+                    Go <span className="text-accent-2">OPEN-SOURCE!</span>
+                  </h2>
+                  <img
+                    src={greenLine}
+                    alt="Green Line"
+                    className=" relative w-60 self-center left-5"
+                  />
                 </div>
-              )
-            case "kontak":
-              return(
-                <div>Hubungi Kami</div>
-              )
-          }})()}
+              );
+            case 'kontak':
+              return <div>Hubungi Kami</div>;
+          }
+        })()}
       </h2>
     </div>
   );
