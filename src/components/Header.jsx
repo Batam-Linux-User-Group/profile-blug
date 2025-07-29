a0c4b8743b8923311089d27b0c326dbaf2dd8f0b
Bayu Maulana <bayufransdo@gmail.com>
2025-07-29 17:32:02 +0700

    udpate link osc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const location = useLocation();
@@ -20,25 +20,31 @@ const Header = () => {
     <header className="w-full py-3 sticky top-0 z-50 bg-white shadow-[0_4px_12px_-2px_rgba(255,229,103,0.3)]">
       <nav className="lg:max-w-screen-lg mx-auto flex flex-wrap items-center justify-between">
         {/* Logo */}
-        <div className='flex-1 flex justify-start'>
+        <div className="flex-1 flex justify-start">
           <img src="nav-logo.svg" alt="Logo BLUG" />
         </div>
         {/* Nav Items */}
         <ul className="flex-2 flex justify-center items-center gap-10">
-          {
-            navItems.map((item, index) => (
-              <NavItems
-                key={index}
-                label={item.label}
-                link={item.link}
-                isActive={isActive(item.link)}
-              />
-            ))
-          }
+          {navItems.map((item, index) => (
+            <NavItems
+              key={index}
+              label={item.label}
+              link={item.link}
+              isActive={isActive(item.link)}
+            />
+          ))}
         </ul>
         {/* OSC Regist Button */}
         <div className="flex-1 flex justify-end text-secondary-dark font-bold group">
-          <a href="/osc" className="bg-primary border-4 border-secondary-dark px-4 py-2 rounded-full group-hover:scale-105 transition-all delay-300 text-sm">
+          <a
+            href="/osc"
+            target="_self"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/osc';
+            }}
+            className="bg-primary border-4 border-secondary-dark px-4 py-2 rounded-full group-hover:scale-105 transition-all delay-300 text-sm"
+          >
             Daftar OSC 2025
           </a>
         </div>
@@ -50,13 +56,14 @@ const Header = () => {
 const NavItems = ({ label, link, isActive }) => {
   return (
     <li className="text-secondary-dark relative group flex justify-center">
-      <Link
-        to={link}
-        className={`${isActive ? 'font-bold' : 'font-normal'}`}
-      >
+      <Link to={link} className={`${isActive ? 'font-bold' : 'font-normal'}`}>
         {label}
       </Link>
-      <div className={`absolute -bottom-1 bg-primary transition-all delay-150 h-1 ${isActive ? 'w-1/2' : 'w-0 group-hover:w-1/2'}`} />
+      <div
+        className={`absolute -bottom-1 bg-primary transition-all delay-150 h-1 ${
+          isActive ? 'w-1/2' : 'w-0 group-hover:w-1/2'
+        }`}
+      />
     </li>
   );
 };
