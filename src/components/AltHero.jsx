32c9bddac4d0b956ffa41a99d84beaaa830d966e
EliteHero <bagassatrio807@gmail.com>
2025-08-11 18:02:58 +0700

    Replace AOS on Tentang Page with Motion

--- src/components/AltHero.jsx
+++ src/components/AltHero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import background from '../assets/HubungiPage/background.png'; // Import tetap di sini
 import greenLine from '../assets/AltHero/line_green.png'; // Import tetap di sini
+import FlipDown from './Animation/FlipDown';
+import ZoomIn from './Animation/ZoomIn';
 
 const AltHero = ({ label, title, addStyle }) => {
   return (
@@ -12,34 +14,35 @@ const AltHero = ({ label, title, addStyle }) => {
         backgroundSize: 'cover',
       }}
     >
-      <div
-        className="bg-yellow-400 bg-opacity-60 inline-block px-4 py-1 text-sm font-bold"
-        data-aos="flip-down"
-      >
-        {label}
-      </div>
-      <h2 className="text-3xl md:text-4xl font-black mt-4" data-aos="zoom-in">
-        {title} <br />
-        {(() => {
-          switch (addStyle) {
-            case 'tentang':
-              return (
-                <div className="flex flex-col">
-                  <h2>
-                    Go <span className="text-accent-2">OPEN-SOURCE!</span>
-                  </h2>
-                  <img
-                    src={greenLine}
-                    alt="Green Line"
-                    className=" relative w-60 self-center left-5"
-                  />
-                </div>
-              );
-            case 'kontak':
-              return <div>Hubungi Kami</div>;
-          }
-        })()}
-      </h2>
+      <FlipDown>
+        <div className="bg-yellow-400 bg-opacity-60 inline-block px-4 py-1 text-sm font-bold">
+          {label}
+        </div>
+      </FlipDown>
+      <ZoomIn delay={0.3}>
+        <h2 className="text-3xl md:text-4xl font-black mt-4">
+          {title} <br />
+          {(() => {
+            switch (addStyle) {
+              case 'tentang':
+                return (
+                  <div className="flex flex-col">
+                    <h2>
+                      Go <span className="text-accent-2">OPEN-SOURCE!</span>
+                    </h2>
+                    <img
+                      src={greenLine}
+                      alt="Green Line"
+                      className=" relative w-60 self-center left-5"
+                    />
+                  </div>
+                );
+              case 'kontak':
+                return <div>Hubungi Kami</div>;
+            }
+          })()}
+        </h2>
+      </ZoomIn>
     </div>
   );
 };
--- src/components/Tentang/Sejarah.jsx
+++ src/components/Tentang/Sejarah.jsx
@@ -1,68 +1,73 @@
 import React from 'react';
+import { motion, easeOut } from 'motion/react';
 import { poltek } from '../../assets/Tentang';
+import FlipUp from '../Animation/FlipUp';
+import FadeRight from '../Animation/FadeRight';
+import FadeLeft from '../Animation/FadeLeft';
+import ZoomOut from '../Animation/ZoomOut';
 
 const Sejarah = () => {
   return (
     <section className="bg-[#2A2A2A] text-white py-16 px-4 md:px-8 font-gilroy">
       <div className="max-w-5xl mx-auto flex flex-col gap-8 items-center">
         {/* Gambar */}
-        <div className="relative rounded-[30px] overflow-hidden w-full max-w-5xl">
-          <img
-            src={poltek}
-            alt="Poltek"
-            className="w-full h-auto object-cove"
-          />
-          <div className="absolute inset-0 bg-primary opacity-50" />
-        </div>
+        <ZoomOut delay={0.5}>
+          <div className="relative rounded-[30px] overflow-hidden w-full max-w-5xl">
+            <img
+              src={poltek}
+              alt="Poltek"
+              className="w-full h-auto object-cove"
+            />
+            <div className="absolute inset-0 bg-primary opacity-50" />
+          </div>
+        </ZoomOut>
 
         {/* Konten */}
         <div className="text-left w-full max-w-5xl">
           {/* Label */}
-          <p
-            className="text-sm font-semibold text-black bg-primary inline-block px-6 py-1 rounded-full mb-2"
-            data-aos="flip-up"
-          >
-            Penasaran ga sih?
-          </p>
+          <FlipUp>
+            <p className="text-sm font-semibold text-black bg-primary inline-block px-6 py-1 rounded-full mb-2">
+              Penasaran ga sih?
+            </p>
+          </FlipUp>
           <br />
           {/* Judul */}
-          <h2
-            className="text-3xl md:text-4xl font-gilroy font-extrabold mb-8 inline-block border-b-[5px] border-primary pb-1"
-            data-aos="fade-right"
-          >
-            Sejarah terbentuknya Batam Linux User Group
-          </h2>
+          <FadeRight>
+            <h2 className="text-3xl md:text-4xl font-gilroy font-extrabold mb-8 inline-block border-b-[5px] border-primary pb-1">
+              Sejarah terbentuknya Batam Linux User Group
+            </h2>
+          </FadeRight>
 
           <div className="flex flex-col md:flex-row gap-8 items-start">
             {/* Paragraf Kiri */}
             <div className="md:w-[50%]">
-              <p
-                className="text-tertiary leading-relaxed text-justify"
-                data-aos="fade-left"
-                data-aos-delay="300"
-              >
-                Batam Linux User Group <b>(BLUG)</b> merupakan{' '}
-                <b>Komunitas Pecinta Linux Indonesia (KPLI) </b>
-                yang berlokasi di Kota Batam sebagai wadah yang digunakan untuk
-                kegiatan pembelajaran dan pengembangan sistem operasi{' '}
-                <b>GNU/Linux </b>
-                dan Open Source Software bagi masyarakat Kota Batam umumnya dan
-                mahasiswa Politeknik Negeri Batam. Berdiri pada tanggal 27
-                September 2003 di mana pada saat itu masih berstatus sebagai
-                komunitas dan bergabung sebagai ORMAWA di Politeknik Negeri
-                Batam pada tahun 2008 dan telah mengadakan berbagai kegiatan
-                internal maupun eksternal.
-              </p>
+              <FadeLeft>
+                <p className="text-tertiary leading-relaxed text-justify">
+                  Batam Linux User Group <b>(BLUG)</b> merupakan{' '}
+                  <b>Komunitas Pecinta Linux Indonesia (KPLI) </b>
+                  yang berlokasi di Kota Batam sebagai wadah yang digunakan untuk
+                  kegiatan pembelajaran dan pengembangan sistem operasi{' '}
+                  <b>GNU/Linux </b>
+                  dan Open Source Software bagi masyarakat Kota Batam umumnya dan
+                  mahasiswa Politeknik Negeri Batam. Berdiri pada tanggal 27
+                  September 2003 di mana pada saat itu masih berstatus sebagai
+                  komunitas dan bergabung sebagai ORMAWA di Politeknik Negeri
+                  Batam pada tahun 2008 dan telah mengadakan berbagai kegiatan
+                  internal maupun eksternal.
+                </p>
+              </FadeLeft>
             </div>
 
             {/* Garis Vertikal PEMISAH (abu tipis) */}
             <div className="hidden md:block w-px bg-gray-300  self-stretch" />
 
             {/* Quote + List */}
-            <div
+            <motion.div
               className="md:w-[45%] flex flex-col gap-6"
-              data-aos="fade-right"
-              data-aos-delay="500"
+              initial={{opacity: 0, x: -50}}
+              whileInView={{opacity: 1, x: 0}}
+              viewport={{once: true, amount:0.5}}
+              transition={{ duration: 0.5, ease: easeOut }}
             >
               {/* Quote Card */}
               <div className="bg-primary text-black font-black  text-2xl rounded-xl py-8 px-10 shadow-xl font-gilroyHeavy text-center">
@@ -92,7 +97,7 @@ const Sejarah = () => {
                   </li>
                 </ul>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
