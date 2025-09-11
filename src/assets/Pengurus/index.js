c6232fc727a1ac3b4825d5889ce536719e5ee862
Bayu Maulana <bayufransdo@gmail.com>
2025-09-11 11:42:46 +0700

    fix typo

--- src/assets/Pengurus/index.js
+++ src/assets/Pengurus/index.js
@@ -2,7 +2,7 @@ import bgInti from './bg-inti.png'
 import intiImage from './inti-image.webp'
 import lurus from './lurus.png'
 import poltek from './poltek.webp'
-import inti from './team/Inti.png'
+import inti from './team/hrd.png'
 import medin from "./team/medin.png"
 import programming from "./team/Programming.png"
 import siber from "./team/Siber.png"
